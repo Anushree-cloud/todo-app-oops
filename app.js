44ff093c0186d8d4469048cd42fb3f3a2ca73bf0
unknown <chakrabortyanushree1999@gmail.com>
2021-07-28 16:46:51 +0530

    removed errors

--- app.js
+++ app.js
@@ -1,56 +1,3 @@
-/* Create a TodoApp class
-
-It will used as like this new TodoApp(document.getElementById("app"))
-
-This will generate a todo app inside the app div
-
-Hint
-====
-
-class TodoApp{
-   constructor(el){
-        this.rootElement = el
-
-        this.render()
-   }
-
-   render(){ render the todo app using inner html & add eventlistners to inputs & buttons }
-
-   addTodo() { add logic for adding todo into list}
-
-   deleteTodo(element) {delete todo element}
-} */
-
-// class TodoApp {
-//     constructor(){
-//         const button = document.getElementById('todoBtn')
-//         button.addEventListener('click', () => { this.render() })
-        
-//     }
-
-//     render(){
-//         const ul = document.getElementById('showTodo')
-//         const input = document.getElementById('todoInp')
-//         let li = this.createItem()
-//         li.innerText = input.value
-//         console.log(input.value);
-//         ul.appendChild(li)
-//         input.value = ""
-//     }
-
-//     addTodo() {
-        
-//     }
-
-//     createItem() {
-//         let li = document.createElement('li')
-//         li.id = 'listItem'
-//         return li
-//     }
-// }
-
-
-
 class TodoApp{
     constructor(el){
         this.rootElement = el
@@ -78,7 +25,6 @@ class TodoApp{
         ul.appendChild(li)
         input.value = ""
         this.deleteTodo(li)
-        //add new list element with input.value into the this.rootElement.querySelector("ul") element
     }
 
     deleteTodo(element) {
