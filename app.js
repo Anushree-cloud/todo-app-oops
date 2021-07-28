a4ebc515176b37d3355c91dea0d03075236cc97b
unknown <chakrabortyanushree1999@gmail.com>
2021-07-28 19:12:16 +0530

    added styling and alert-box

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ class TodoApp{
     }
 
     render(){ 
-        this.rootElement.innerHTML = `<input type="text" placeholder="add task"/>
-        <button>Add</button>
-        <ul></ul>`
+        this.rootElement.innerHTML = `<div id="todo-app">
+        <h1>Add your tasks here</h1>
+            <div div="todo-app-content">
+                <input type="text" placeholder="add task"/>
+                <button>Add</button>
+                <ul></ul>
+            </div>
+        </div>`
 
         const addButton = this.rootElement.querySelector("button")
         addButton.addEventListener('click', () => {
@@ -19,12 +24,16 @@ class TodoApp{
         const input = this.rootElement.querySelector("input")
         const ul = this.rootElement.querySelector("ul")
         let li = document.createElement('li')
-        li.innerText = input.value
-        li.style.cursor= "pointer"
-        console.log(input.value);
-        ul.appendChild(li)
-        input.value = ""
-        this.addDeleteListener(li)
+        if (input.value == "")
+            alert("First write something!")
+        else {
+            li.innerText = input.value
+            li.style.cursor= "pointer"
+            console.log(input.value);
+            ul.appendChild(li)
+            input.value = ""
+            this.addDeleteListener(li)
+        }
     }
 
     addDeleteListener(element) {
